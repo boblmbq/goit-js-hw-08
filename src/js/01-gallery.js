1f1b97b210e2e27757ec9321c8302eea09e5f665
boblmbq <sichkardanylo@gmail.com>
2023-07-12 11:59:43 +0200

    al trash deleted and first task added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulEl = document.querySelector('.gallery');
+
+//? making markup in ul
+const ulMarkup = galleryItems
+  .map(e => {
+    return `<li class="gallery__item">
+             <a class="gallery__link" href="${e.original}">
+                <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+            </a>
+          </li>`;
+  })
+  .join(' ');
+ulEl.insertAdjacentHTML('beforeend', ulMarkup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
