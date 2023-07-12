677e8d6ecc53e23ea4e01a5b07e83aad74c546eb
boblmbq <sichkardanylo@gmail.com>
2023-07-12 14:11:00 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Player(iframe);
+
+const videoLastDuration = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(videoLastDuration, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
