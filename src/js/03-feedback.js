b3037a38dd804fd87ae4708e1bb4545c7af14127
boblmbq <sichkardanylo@gmail.com>
2023-07-13 09:30:09 +0200

    third task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+const INPUT_VALUES = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
 
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+const arrOfValues = {};
+
+function onFormInput(e) {
+  arrOfValues[e.target.name] = e.target.value;
+  localStorage.setItem(INPUT_VALUES, JSON.stringify(arrOfValues));
+}
+
+onUnexpectedRestart();
+
+function onFormSubmit(e) {
+  console.log(arrOfValues);
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(INPUT_VALUES);
+}
+
+function onUnexpectedRestart() {
+  const LSValues = localStorage.getItem(INPUT_VALUES);
+
+  if (LSValues) {
+    const arrOfJson = JSON.parse(LSValues);
+
+    if (arrOfJson.email !== undefined) {
+      refs.input.value = arrOfJson.email;
+    }
+    if (arrOfJson.message !== undefined) {
+      refs.textarea.value = arrOfJson.message;
+    }
+  }
+}
